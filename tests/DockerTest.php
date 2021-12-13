88377d771d324c0133154d784cd5b6b3654c2b61
zmitic <mitke013@gmail.com>
2021-12-13 17:49:59 +0100

    Docker task

---
+++ tests/DockerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LML\SDK\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class DockerTest extends KernelTestCase
+{
+    public function testDocker(): void
+    {
+        self::assertTrue(true);
+    }
+}
