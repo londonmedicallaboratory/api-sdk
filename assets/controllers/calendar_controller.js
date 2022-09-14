03db422024b392c98d231b69104da00f789f75d2
zeljko <mitke013@gmail.com>
2022-09-14 16:49:29 +0200

    symfony-ux implementation

---
+++ assets/controllers/calendar_controller.js
@@ -0,0 +1,8 @@
+import {Controller} from '@hotwired/stimulus';
+
+/* stimulusFetch: 'lazy' */
+export default class calender_controller extends Controller {
+    connect() {
+        this.element.textContent = 'vucicu pederu';
+    }
+}
