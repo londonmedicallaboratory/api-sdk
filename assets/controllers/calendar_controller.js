4145eb9996cdd7871e1bedb6b7483ba5f7fa5eab
zeljko <mitke013@gmail.com>
2022-09-16 10:42:37 +0200

    Fixed package name

--- assets/controllers/calendar_controller.js
+++ assets/controllers/calendar_controller.js
@@ -3,6 +3,6 @@ import {Controller} from '@hotwired/stimulus';
 /* stimulusFetch: 'lazy' */
 export default class calender_controller extends Controller {
     connect() {
-        this.element.textContent = 'vucicu pederu';
+        this.element.textContent = 'vucicu pederu2';
     }
 }
