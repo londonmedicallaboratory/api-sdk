41b043f1cfb8577ab2ea75fb431f41a1343dbab5
Zeljko Mitic <mitke013@gmail.com>
2024-01-10 18:18:44 +0100

    Psalm 5 (#86)
    * Psalm5
    
    * Psalm5
    
    * Minor fixes
    
    * Dependencies update
    
    * Fixed deprecations in Symfony
    6.4
    
    * Minor fixes
    
    * Minor fixes

--- src/Repository/Basket/BasketRepository.php
+++ src/Repository/Basket/BasketRepository.php
@@ -198,7 +198,7 @@ class BasketRepository extends AbstractRepository
     }
 
     /**
-     * @param ?TAppointment $initialAppointment
+     * @param ?array{brand_id: string, starts_at?: ?string, ends_at?: ?string, time_id?: ?string, type: 'brand_location'|'home_visit_phlebotomist', id?: ?string, patient_id?: ?string, confirmed?: ?bool} $initialAppointment
      */
     private function getInitialAppointment(?array $initialAppointment): ?Appointment
     {
