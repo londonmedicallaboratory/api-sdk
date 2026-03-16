ceda3bf11aefe25a6fd0eded83e2a71a2dba11b0
Charles Astwood <charles@astwood-online.com>
2026-03-16 21:06:57 +0000

    Upgrade to Symfony 7: rewrite EntityParamConverter, remove sensio, widen constraints

--- src/ArgumentValueResolver/EntityParamConverter.php
+++ src/ArgumentValueResolver/EntityParamConverter.php
@@ -12,72 +12,60 @@ use LML\SDK\Service\API\EntityManager;
 use LML\SDK\Util\ReflectionAttributeReader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use function is_a;
 use function sprintf;
 
 /**
- * Allows param conversion of model, similar to
+ * Allows param conversion of model, similar to Doctrine's entity value resolver.
  *
- * @see https://github.com/sensiolabs/SensioFrameworkExtraBundle/blob/master/src/Request/ParamConverter/DoctrineParamConverter.php
- *
- * Only slug is supported, and no conversion.
+ * Only slug and id are supported.
  */
-class EntityParamConverter implements ParamConverterInterface
+class EntityParamConverter implements ValueResolverInterface
 {
     public function __construct(
         private EntityManager $modelManager,
-    )
-    {
+    ) {
     }
 
-    public function supports(ParamConverter $configuration): bool
+    /**
+     * @return iterable<ModelInterface>
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        $class = $configuration->getClass();
+        $class = $argument->getType();
         if (!$class) {
-            return false;
+            return [];
         }
         if (!is_a($class, ModelInterface::class, true)) {
-            return false;
+            return [];
         }
-        /**
-         * Basket must never be loaded via `find` method, let @see ActiveBasketResolver take care of it
-         */
+        // Basket must never be loaded via `find` method, let ActiveBasketResolver take care of it
         if (is_a($class, Basket::class, true)) {
-            return false;
+            return [];
         }
         $entityAttribute = ReflectionAttributeReader::getAttribute($class, Entity::class);
+        if (!$entityAttribute) {
+            return [];
+        }
 
-        return (bool)$entityAttribute;
-    }
-
-    public function apply(Request $request, ParamConverter $configuration): bool
-    {
-        $name = $configuration->getName();
-        $id = (string)$request->attributes->get('id');
-        $slug = (string)$request->attributes->get('slug');
+        $id = (string) $request->attributes->get('id');
+        $slug = (string) $request->attributes->get('slug');
         if (!$slug && !$id) {
-            return false;
+            return [];
         }
-        $class = $configuration->getClass() ?? throw new RuntimeException('This must never happen.');
-        if (!is_a($class, ModelInterface::class, true)) {
-            return false;
-        }
-        $attribute = ReflectionAttributeReader::getAttribute($class, Entity::class) ?? throw new RuntimeException('This must never happen.');
-        $repository = $this->modelManager->getRepository($attribute->getRepositoryClass());
+
+        $repository = $this->modelManager->getRepository($entityAttribute->getRepositoryClass());
 
         if ($id) {
             $model = $repository->find(id: $id, await: true) ?? throw new NotFoundHttpException(sprintf('ID "%s" not found.', $id));
-            $request->attributes->set($name, $model);
+            yield $model;
 
-            return true;
+            return;
         }
 
         $model = $repository->findOneBySlug($slug, true) ?? throw new NotFoundHttpException(sprintf('Slug "%s" not found.', $slug));
-
-        $request->attributes->set($name, $model);
-
-        return true;
+        yield $model;
     }
 }
